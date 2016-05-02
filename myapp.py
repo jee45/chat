d66c5c01664178b98d64d35679b6bdbc20886e77
ad <jee45@txstate.edu>
2016-05-02 09:50:16 -0500

    a

--- myapp.py
+++ myapp.py
@@ -19,6 +19,8 @@ io =   flask_socketio.SocketIO(app)
 urlkeys = {}
 rooms={}
 
+game={}
+
 
 @app.route('/')
 def index():
@@ -40,7 +42,8 @@ def submitNewChat():
 
 
     # create a url hash for the submitted topic
-    keyFromUrl = base64.urlsafe_b64encode(uuid.uuid4().bytes)[:12].decode('ascii')
+    #keyFromUrl = base64.urlsafe_b64encode(uuid.uuid4().bytes)[:12].decode('ascii')
+    keyFromUrl = topic
 
 
     # add denote this key is a room url
@@ -114,6 +117,72 @@ def back(keyFromUrl):
 
 
 
+@io.on('submitmove' )
+def submitMove(data):
+    print("-------------------------------")
+    print(data)
+    print(flask_socketio.rooms())
+    room = flask.session['room']
+    print(room)
+
+    if game[flask.session['room']] :
+        if flask.session['sid'] in game[flask.session['room']]:
+            game[flask.session['room']].append(data)
+
+    if data['turn'] =='X':
+        print('############################### emitting to O')
+
+
+        io.emit('otherplayermademove', {"row":data['row'], "col":data['col'] }, room =game[room][0])
+
+    elif data['turn'] =='O':
+        print('############################### emitting to X')
+        io.emit('otherplayermademove', {"row":data['row'], "col":data['col'] }, room = game[room][1] )
+
+    else:
+        print('what the hell are you trying to pull? ')
+
+    io.emit('observing', {"row":data['row'], "col":data['col'], "turn": data['turn']}, room = "observing"+flask.session['room'] )
+
+
+
+
+
+
+
+
+@io.on('disconnect' )
+def disconnect():
+    print(flask_socketio.rooms())
+    print("someone disconnected. ")
+
+
+    for room in flask_socketio.rooms():
+        io.emit('someoneleftaroom', room=room)
+
+        #flask_socketio.leave_room(room)
+
+        if room in rooms:
+            print("removing user from room")
+            print (rooms[room])
+            rooms[room].remove(flask.session['name'])
+            print (rooms[room])
+
+        if room in game:
+            if flask.session['sid'] in game[room]:
+                print('removing player from game.')
+                print(game[room])
+                game[room].remove(flask.session['sid'])
+                io.emit('playerleftgame', room=room)
+                print(game[room])
+
+
+
+
+
+
+
+
 @io.on('connect' )
 def connect():
 
@@ -124,8 +193,9 @@ def connect():
 
     print(socketId)
 
+
     # how to send to just them?
-    io.emit('connect',  room = socketId[0] )
+
 
     print('emitting to connect so user can just rejoin the room')
     print('user is in the  rooms: ', flask_socketio.rooms())
@@ -137,21 +207,27 @@ def enterchat(data):
 
     flask.session['name'] = data['name']
 
-
-    print('entering chat  in io: .... ')
-    print(data)
-
-    print('data sid :', data['sid'])
-    print('sess sid :', flask.session['sid'])
-
-    print('data room :', data['room'])
-    print('sess room :', flask.session['room'])
-
-    print('data name :', data['name'])
-    print('sess name :', flask.session['name'])
+    #
+    # print('entering chat  in io: .... ')
+    # print(data)
+    #
+    # print('data sid :', data['sid'])
+    # print('sess sid :', flask.session['sid'])
+    #
+    # print('data room :', data['room'])
+    # print('sess room :', flask.session['room'])
+    #
+    # print('data name :', data['name'])
+    # print('sess name :', flask.session['name'])
 
     # >>>> >>>> >>>> >>>> >>>> >>>> >>>> >>>>
     # >>>> >>>> >>>> >>>> >>>> >>>> >>>> >>>>
+
+
+
+
+
+
 
     # if the room sent is already in thier flask_socketio.rooms()
     if data['room'] in flask_socketio.rooms():
@@ -161,6 +237,8 @@ def enterchat(data):
 
     #else
     else:
+
+
 
 
 
@@ -175,11 +253,15 @@ def enterchat(data):
         io.emit("joined", { "name": data['name']} , room=flask.session['sid'] )
 
 
+
+
         # if they are already in the list of users in the  room array.
         print('listing rooms: ' , rooms)
         print('is the room sent one of them? : ' , data['room'])
 
         print(data)
+
+
 
         #if the room exists
         if data['room'] in rooms:
@@ -211,9 +293,16 @@ def enterchat(data):
                 # notify the room that i have joined
                 io.emit("user-joined", { "name": data['name']}, room=data['room']  )
 
-        else:
-            print('the room doesnt exist')
 
+
+
+
+
+
+        else:
+
+
+            print('the room doesnt exist')
             # is there something to do id the room doesnt exist yet?
             # create it in the list ?
             # add this user to the list ?
@@ -225,6 +314,37 @@ def enterchat(data):
 
             # notify the room that they have joined
             io.emit("user-joined", { "name": data['name']}, room=data['room']  )
+
+
+
+
+    if data['room'] not in game:
+        game[data['room']] = []
+        game[data['room']].append(data['sid'])
+        for player in game[data['room']]:
+            print(player)
+
+
+    elif len(game[data['room']])==1:
+        game[data['room']].append(data['sid'])
+        print('players in the game room. ',game[data['room']])
+        print('i am in the rooms: . ', flask_socketio.rooms())
+
+
+
+        io.emit('anotherplayerentered', {'myTurnWillBe':'O'}, room = game[data['room']][0] )
+        io.emit('anotherplayerentered', {'myTurnWillBe':'X'}, room = game[data['room']][1] )
+
+    else:
+        #player can register to recieve the data to to observe th game?
+
+        flask_socketio.join_room("observing" + data['room'])
+        for eachmove in game[data['room']]:
+                io.emit('observing', eachmove, room = "observing"+flask.session['room'] )
+
+
+
+
 
 
 
--- static/chat.js
+++ static/chat.js
@@ -1,22 +1,23 @@
 /**
  * Created by ad on 4/22/16.
  */
-(function(n, e) {
+
+$(document).ready(function() {
     var a = io();
 
+    var statusDisplay = $("#status");
+    //var waiting = statusDisplay.attr("data-waiting");
+    var waiting = true;
+    var myTurnWillBe = null;
 
     var o = $(".chatwindow");
-
 
 
     var t = o.attr("data-key");
     var s = o.attr("data-sid");
 
 
-
-
-
-    $(".join-form").on("submit", function(n) {
+    $(".join-form").on("submit", function (n) {
         n.preventDefault();
         var e = $("#join-nick").val();
         if (e) {
@@ -34,11 +35,7 @@
     });
 
 
-
-
-
-
-    $(".chat-input").on("submit", function(n) {
+    $(".chat-input").on("submit", function (n) {
         n.preventDefault();
         var e = $("#in-msg").val().trim();
         if (!e)
@@ -56,8 +53,7 @@
 
 
 
-
-    a.on("connect", function() {
+    a.on("connect", function () {
         console.log("connected");
 
         var n = o.attr("data-nick");
@@ -70,16 +66,13 @@
                 sid: s,
                 room: t,
                 name: n
-            })
+            });
 
         }
     });
 
 
-
-
-
-    a.on("joined", function(n) {
+    a.on("joined", function (n) {
         console.log(n);
         var e = n.name;
         console.log("joined %s as %s", t, e);
@@ -89,14 +82,7 @@
     });
 
 
-
-
-
-
-
-
-
-    a.on("new-chat", function(n) {
+    a.on("new-chat", function (n) {
         console.log('enterying new chat');
         $("<li>").addClass("message").addClass("mid").append($("<span>").addClass("user").text(n.sender)).append(": ").append($("<span>").addClass("text").text(n.message)).prependTo($(".chat-messages"))
 
@@ -104,17 +90,359 @@
     });
 
 
-
-
-    a.on("user-joined", function(n) {
+    a.on("user-joined", function (n) {
         $("<li>").addClass("joined").append($("<span>").addClass("user").text(n.name)).append(" has joined the chat").appendTo($(".chat-messages"));
         $("<li>").text(n.name).appendTo($(".roster-list"))
     });
 
 
 
+    /* a game board is a 3x3 array */
 
-    e[""] = n
-})({}, function() {
-    return this
-}());
+    var board = [
+        [null, null, null, null, null, null],
+        [null, null, null, null, null, null],
+        [null, null, null, null, null, null],
+
+        [null, null, null, null, null, null],
+        [null, null, null, null, null, null],
+        [null, null, null, null, null, null]
+    ];
+
+    var turn = null;
+    //var turn = 'X';
+
+    var winner = null;
+
+    function getBottomOfColumn(row, col) {
+
+        for (var i = 5; i > -1; i--) {
+            console.log(board[i][col]);
+
+            if (board[i][col] == null) {
+
+                return i;
+
+            }
+
+        }
+        return col;
+    }
+
+
+    /** Update the state of a particular cell. */
+    function update(row, col, x_or_o) {
+
+
+        board[row][col] = x_or_o;
+        //board[bottomRow][col] = x_or_o;
+        console.log(" >>>>>>>>  >>>>>>>> marking new square ", row, col);
+        // update the cell's visible state
+
+
+        console.log("attempting to submitMove")
+
+
+        $('#cell-' + row + '-' + col).text(x_or_o).removeClass('empty').addClass(x_or_o.toLowerCase());
+        //$('#cell-' + bottomRow + '-' + col).text(x_or_o).removeClass('empty').addClass(x_or_o.toLowerCase());
+
+
+    }
+
+
+
+    function changePlayer() {
+
+
+        console.log('switching player.,.')
+
+        var next;
+        switch (turn) {
+            case 'X':
+                next = 'O';
+                break;
+            case 'O':
+                next = 'X';
+                break;
+            default:
+                if (winner === null) {
+                    next = 'X';
+                } else {
+                    next = null;
+                }
+        }
+        turn = next;
+
+        if (next) {
+
+            if(next==myTurnWillBe){
+                // update the status!
+                console.log('player %s turn', turn);
+                $('#status').text('Player ' + turn + ', it is your turn!');
+                waiting = false;
+
+
+            }else{
+                console.log('player %s turn', turn);
+                $('#status').text('Player ' + turn + ', waiting for them to play.');
+                waiting = true;
+            }
+
+
+        } else {
+            $('#status').text('Game over.');
+        }
+    }
+
+
+    function checkWinner() {
+        var status;
+        var empties = 0;
+        var i, j;
+
+        // count empties
+        for (i = 0; i < 5; i++) {
+            for (j = 0; j < 5; j++) {
+                if (board[i][j] === null) {
+                    empties += 1;
+                }
+            }
+        }
+
+
+        //rows cols diags
+        for (i = 1; i < 5; i++) {
+
+            for (j = 1; j < 5; j++) {
+
+
+                if (board[i][j] && board[i][j] === board[i - 1][j - 1] && board[i][j] === board[i + 1][j + 1]) {
+                    console.log('sloping down and right')
+
+                    status = {
+                        winner: board[i][j],
+                        rows: [i - 1, i, i + 1],
+                        cols: [j - 1, j, j + 1]
+                    };
+
+
+                } else if (board[i][j] && board[i][j] === board[i - 1][j + 1] && board[i][j] === board[i + 1][j - 1]) {
+                    console.log('sloping down and left')
+                    status = {
+                        winner: board[i][j],
+                        rows: [i - 1, i, i + 1],
+                        cols: [j + 1, j, j - 1]
+                    };
+
+
+                } else if (board[i][j] && board[i][j] === board[i - 1][j] && board[i][j] === board[i + 1][j]) {
+                    console.log('up and down')
+
+                    status = {
+                        winner: board[i][j],
+                        rows: [i - 1, i, i + 1],
+                        cols: [j, j, j]
+                    };
+
+                } else if (board[i][j] && board[i][j] === board[i][j - 1] && board[i][j] === board[i][j + 1]) {
+                    console.log('side ways')
+
+                    status = {
+                        winner: board[i][j],
+                        rows: [i, i, i],
+                        cols: [j - 1, j, j + 1]
+                    };
+
+                }
+
+
+            }
+        }
+
+        //edges
+        for (i = 0; i < 4; i++) {
+
+            if (board[0][i] && board[0][i] === board[0][i + 1] && board[0][i] === board[0][i + 2]) {
+
+                console.log('top edge')
+                status = {
+                    winner: board[0][i],
+                    rows: [0, 0, 0],
+                    cols: [i, i + 1, i + 2]
+                };
+
+
+            } else if (board[5][i] && board[5][i] === board[5][i + 1] && board[5][i] === board[5][i + 2]) {
+                console.log('bottom edge')
+                status = {
+                    winner: board[5][i],
+                    rows: [5, 5, 5],
+                    cols: [i, i + 1, i + 2]
+                };
+
+
+            } else if (board[i][0] && board[i][0] === board[i + 1][0] && board[i][0] === board[i + 2][0]) {
+
+
+                console.log('left edge')
+
+                status = {
+                    winner: board[i][0],
+                    rows: [i, i + 1, i + 2],
+                    cols: [0, 0, 0]
+                };
+
+
+            } else if (board[i][5] && board[i][5] === board[i + 1][5] && board[i][5] === board[i + 2][5]) {
+                console.log('right edge')
+                status = {
+                    winner: board[i][5],
+                    rows: [i, i + 1, i + 2],
+                    cols: [5, 5, 5]
+                };
+
+
+            }
+
+        }
+
+
+        if (status) {
+            // we have a winner
+            return status;
+        } else if (empties) {
+            // game still goes
+            return null;
+        } else {
+            // no empties, no winner, game over
+            return {
+                winner: 'draw'
+            };
+        }
+
+
+    }
+
+
+    // game over
+    function finishGame() {
+        waiting=true;
+
+        if (winner.winner === 'draw') {
+            $('#status').text('The game ends in a draw.');
+        } else {
+            $('#status').text('Player ' + winner.winner + ' has won!');
+            // now we need to mark!
+            for (var i = 0; i < 5; i++) {
+                var col = winner.cols[i];
+                var row = winner.rows[i];
+                var id = '#cell-' + row + '-' + col;
+                $(id).addClass('winning');
+            }
+        }
+    }
+
+
+    a.on("anotherplayerentered", function (data) {
+        console.log("anotherplayerentered");
+        console.log(data);
+
+        myTurnWillBe = data.myTurnWillBe;
+        changePlayer();
+
+
+    });
+
+
+    a.on("otherplayermademove", function (d) {
+        console.log("otherplayermademove");
+        console.log(d);
+        if(waiting) {
+            makeMove(d.row, d.col)
+        }
+
+    });
+
+    a.on("someoneleftaroom", function () {
+        console.log("someoneleftaroom");
+
+    });
+    a.on("playerleftgame", function () {
+        $('#status').text('a player left the room. the game is over. fart. ');
+    });
+
+    a.on("observing", function (d) {
+        console.log("observing");
+        console.log(d);
+
+
+        waiting=true;
+        turn = d.turn
+        makeMove(d.row, d.col);
+        $('#status').text('you are only observing. it is Player ' + turn + ' turn.');
+
+
+    });
+
+    function makeMove(row, col) {
+
+
+        bottomRow = getBottomOfColumn(row, col);
+
+        if (waiting) {
+            update(bottomRow, col, turn);
+            winner = checkWinner();
+            if (winner) {
+                finishGame();
+            } else {
+                changePlayer();
+            }
+
+
+        } else if (board[bottomRow][col] === null) {
+
+            console.log("attempting to submit")
+
+            a.emit("submitmove", {
+                row: bottomRow,
+                col: col,
+                turn: turn
+            });
+
+
+            update(bottomRow, col, turn);
+            winner = checkWinner();
+            if (winner) {
+                finishGame();
+            } else {
+                changePlayer();
+            }
+
+        } else {
+            // oops - the user clicked an already-used cell
+            console.log('received click in already-played cell (%d,%d)', row, col);
+        }
+    }
+
+
+
+
+
+    $('.cell').on('click', function () {
+
+        if(!waiting ) {
+
+            var row = parseInt($(this).parent().attr('data-row'));
+            var col = parseInt($(this).attr('data-col'));
+
+            makeMove(row, col);
+        }else{
+            console.log('cant play yet, waiting for ther player. ');
+        }
+
+
+
+    });
+
+
+});
